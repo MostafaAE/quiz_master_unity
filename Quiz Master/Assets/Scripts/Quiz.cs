8a0f91a0f94efb616ce4ffdbdc86a58064a1e7dd
Mostafa Ayman <mostafa.aymanae@gmail.com>
2021-11-22 18:26:11 +0200

    OnAnswerSelected implementation

--- Quiz Master/Assets/Scripts/Quiz.cs
+++ Quiz Master/Assets/Scripts/Quiz.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using TMPro;
+using UnityEngine.UI;
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons = new GameObject[4];
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
+
     void Start()
     {
         questionText.text = question.GetQuestion(); 
@@ -16,8 +20,26 @@ public class Quiz : MonoBehaviour
             TextMeshProUGUI answerText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
             answerText.text = question.GetAnswer(i);
         }
-       
-
 
     }
+
+    public void OnAnswerSelected(int index)
+    {
+        int correctAnswerIndex = question.GetCorrectAnswerIndex();
+        Image buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+        buttonImage.sprite = correctAnswerSprite;
+
+        if (index == correctAnswerIndex)
+        {
+            questionText.text = "Correct!";
+        }
+        else
+        {
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questionText.text = "Sorry, the correct answer was;\n"+ correctAnswer;
+            
+        }
+
+    }
+
 }
